49bbad54dce60fc22699f7c5f4cedc0b56b8d0d8
Vivekanandan Sakthivelu <vivek@bionworks.com>
2020-02-02 18:00:51 +0530

    Update ++
    @vivekanandans

---
+++ src/com/practice/misc/ReverseLinkedList.java
@@ -0,0 +1,24 @@
+package com.practice.misc;
+
+import java.util.Stack;
+
+
+
+public class ReverseLinkedList {
+
+	public static void reverse() {
+
+	}
+
+	public static void main(String[] args)  {
+		Stack<Integer> stack = new Stack<Integer>();
+		System.out.println(stack.capacity());
+		for (int i = 1; i < 100; i++) {
+			stack.push(i);
+		
+		}
+		System.out.println(stack.firstElement()));
+		
+	}
+
+}
