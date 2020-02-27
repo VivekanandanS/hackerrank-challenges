7719ad69c29c2583984d9ea6f72af912da1bd542
Vivekanandan <vivekanandansakthivelu@gmail.com>
2020-02-27 19:28:07 +0530

    Tree | Huffman Decoding

--- src/com/hackerrank/tree/HeightOfBinaryTree.java
+++ src/com/hackerrank/tree/HeightOfBinaryTree.java
@@ -4,12 +4,12 @@ package com.hackerrank.tree;
 import java.util.*;
 import java.io.*;
 
-class Node {
+class Node<T> {
     Node left;
     Node right;
-    int data;
+    T data;
 
-    Node(int data) {
+    Node(T data) {
         this.data = data;
         left = null;
         right = null;
@@ -18,46 +18,13 @@ class Node {
 
 class Solution {
 
-    /*
-    class Node
-        int data;
-        Node left;
-        Node right;
-    */
     public static int height(Node root) {
-        if(null == root)
+        if (null == root)
             return -1;
-        int left =  height(root.left);
-        int right =  height(root.right);
-        return Math.max(left,right) + 1;
+        int left = height(root.left);
+        int right = height(root.right);
+        return Math.max(left, right) + 1;
     }
 
-    public static Node insert(Node root, int data) {
-        if(root == null) {
-            return new Node(data);
-        } else {
-            Node cur;
-            if(data <= root.data) {
-                cur = insert(root.left, data);
-                root.left = cur;
-            } else {
-                cur = insert(root.right, data);
-                root.right = cur;
-            }
-            return root;
-        }
-    }
 
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int t = scan.nextInt();
-        Node root = null;
-        while(t-- > 0) {
-            int data = scan.nextInt();
-            root = insert(root, data);
-        }
-        scan.close();
-        int height = height(root);
-        System.out.println(height);
-    }
 }
