bf4eec01009f8ab8f0ad6ebabbbc40d60c7bff40
Vivekanandan <vivekanandansakthivelu@gmail.com>
2020-03-28 13:03:55 +0530

    Update SwapNodes.java

--- src/com/hackerrank/tree/SwapNodes.java
+++ src/com/hackerrank/tree/SwapNodes.java
@@ -22,15 +22,17 @@ public class SwapNodes {
             depth[i] = new ArrayList<>();
         }
 
-        for (int i = 0; i < indexes.length; i++) {
+        for (int i = 0; (i < indexes.length) && (i < numberOfNodes); i++) {
+
             if (indexes[i][0] > 0)
                 tree[i].left = tree[indexes[i][0] - 1];
 
             if (indexes[i][1] > 0)
                 tree[i].right = tree[indexes[i][1] - 1];
+
         }
 
-        doDepthAnalysis(tree[0], 0);
+        doDepthAnalysis(tree[0], 1);
     }
 
 
@@ -49,24 +51,47 @@ public class SwapNodes {
         construct(indexes);
         for (int i = 0; i < queries.length; i++) {
 
-            List<Node> nodesAtDepth = depth[queries[i] - 1];
+
             int h, k;
             k = h = queries[i];
-            while (h <= numberOfNodes) {
+
+
+            System.out.println("K :" + k);
+            System.out.println("H :" + h);
+            List<Node> nodesAtDepth = null;
+            while ( h  < depth.length  &&  depth[h].size() > 0) {
+                nodesAtDepth = depth[h];
+                System.out.println("Current Depth : " + h);
+                System.out.println(depth[h].toString());
                 nodesAtDepth.forEach(node -> {
+
+                    System.out.println("Current Node " + node);
+                    System.out.println("Current Left" + node.left);
+                    System.out.println("Current Right" + node.right);
+                    System.out.println();
+
                     Node current = tree[node.data - 1];
                     Node temp = current.right;
                     current.right = current.left;
                     current.left = temp;
-                });
-                h += k;
-            }
+                    tree[node.data - 1] = current;
 
+
+                    System.out.println("Current Node " + tree[node.data - 1]);
+                    System.out.println("Current Left" + tree[node.data - 1].left);
+                    System.out.println("Current Right" + tree[node.data - 1].right);
+                    System.out.println();
+
+                });
+
+                h += k;
+                System.out.println("Next Depth : " + h);
+                System.out.println();
+            }
 
             index = 0;
             result[i] = traversal(tree[0], new int[numberOfNodes]);
         }
-
         return result;
     }
 
@@ -93,21 +118,47 @@ public class SwapNodes {
                 {-1, -1}
 
         };
-        int[] queries = new int[]{2};
-        numberOfNodes = 5;
+
+
+        indexes = new int[][]{
+                {2, 3},
+                {-1, -1},
+                {-1, -1},
+
+        };
+
+
+        /*indexes = new int[][]{
+                {2, 3},
+                {4, -1},
+                {5, -1},
+                {6, -1},
+                {7, 8},
+                {-1, 9},
+                {-1, -1},
+                {10, 11},
+                {-1, -1},
+                {-1, -1},
+                {-1, -1}
+        };*/
+
+        int[] queries = new int[]{1, 1};
+
+
+        numberOfNodes = 3;
 
         int[][] result = swapNodes(indexes, queries);
-        /*for (int resultRowItr = 0; resultRowItr < result.length; resultRowItr++) {
+        for (int resultRowItr = 0; resultRowItr < result.length; resultRowItr++) {
 
             for (int resultColumnItr = 0; resultColumnItr < result[resultRowItr].length; resultColumnItr++) {
                 System.out.print(String.valueOf(result[resultRowItr][resultColumnItr]));
                 if (resultColumnItr != result[resultRowItr].length - 1) {
-                    System.out.println(" ");
+                    System.out.print(" ");
                 }
             }
             if (resultRowItr != result.length - 1)
                 System.out.println();
-        }*/
+        }
 
     }
 }
